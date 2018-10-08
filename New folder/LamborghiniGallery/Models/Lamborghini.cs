fb0656fa7cc43a4169f79a475c36cc510c28cccd
DESKTOP-LFO6JU9\Dion <dion.clarke@education.gov.uk>
2018-10-08 15:39:12 +0100

    Added Lamborghini and variant models

---
+++ New folder/LamborghiniGallery/Models/Lamborghini.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LamborghiniGallery.Models
+{
+    public class Lamborghini
+    {
+        public int Id { get; set; }
+        public string CarModel { get; set; }
+        public int YearModel { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Variant[] Variants { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
---
+++ New folder/LamborghiniGallery/Models/Variant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LamborghiniGallery.Models
+{
+    public class Variant
+    {
+        public string ModelName { get; set; }
+        public int ModelYearRelease { get; set; }
+
+
+    }
+}
