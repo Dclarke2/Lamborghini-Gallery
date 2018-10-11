73cb10cc2f438614ac967b4a983b6416b27afa48
DESKTOP-LFO6JU9\Dion <dion.clarke@education.gov.uk>
2018-10-11 11:43:44 +0100

    Updated Lamborghini controller to use repository

--- New folder/LamborghiniGallery/Controllers/LamborghiniController.cs
+++ New folder/LamborghiniGallery/Controllers/LamborghiniController.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LamborghiniGallery.Data;
 using LamborghiniGallery.Models;
 
 namespace LamborghiniGallery.Controllers
 {
     public class LamborghiniController : Controller
     {
-        public ActionResult Detail()
+        private LamborghiniRepository _lamborghiniRepository = null;
+        public LamborghiniController() //Instantiating our repository within in. 
         {
-            
-
-            
+            _lamborghiniRepository = new LamborghiniRepository();  //Instantiating the _lamborghinirepository   
+        }
+        public ActionResult Detail(int? id) //<-- '?' id only accepts an integer atm which isn't 'nullable'. the '?' allows us to pass a nullable type if an int isn't provided. 
+        {                               //id passed into browser 
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var lamborghini = _lamborghiniRepository.GetLamborghini((int)id);//cast (int) allows passage of underlying value from nullable type. So what is should actually be such as int type.  
             return View(lamborghini);
+            
+            
         }
 
 
--- New folder/LamborghiniGallery/Data/LamborghiniRepository.cs
+++ New folder/LamborghiniGallery/Data/LamborghiniRepository.cs
@@ -12,6 +12,7 @@ namespace LamborghiniGallery.Data
         {
             new Lamborghini()
             {
+                Id = 1,
                 CarModel = "Lamborghini Huracan",
                 YearModel = 2018,
                 DescriptionHtml = "<p>The Lamborghini Huracán (Spanish for \"hurricane\"; [uɾaˈkan]) is a sports car manufactured by Italian automotive manufacturer Lamborghini replacing the previous V10 flagship, the Gallardo. The Huracán made its worldwide debut at the 2014 Geneva Auto Show, and was released in the market the second quarter of 2014. The LP 610-4 designation comes from the fact that this car has 610 metric horsepower and 4 wheel drive, while LP stands for \"Longitudinale Posteriore\", which refers to the longitudinal mid-rear engine position. The Huracán was named \"The Supercar of the Year 2014\" by automotive magazine <strong>Top Gear</strong>.</p>",
@@ -35,6 +36,7 @@ namespace LamborghiniGallery.Data
 
             new Lamborghini()
             {
+                Id = 2,
                 CarModel = "Lamborghini Huracan Performante",
                 YearModel = 2017,
                 DescriptionHtml = "<p>Somehow, though, this thing is idiotically fast. In a straight line it will leave a Ferrari 458 Special Needs for dead. And around the Nürburgring, it’s faster than any of the million-quid hypercars. You won’t believe this — very few test drivers that witnessed it did — but it went round in six minutes and 52 seconds. And it’s not just fast. It’s exciting. It may be making the sound of a universe forming and your head may be pinned back against the headrest but you can still feel it blowing gently on the hairs on your arms. This is a car that roars and purrs at the same time. It’s like an Italian tomato — little and bright and so full of taste sensations it makes your eyes go crossed.<strong>Jeremy Clarkson - Top Gear</strong>.</p>",
@@ -57,6 +59,7 @@ namespace LamborghiniGallery.Data
             },
             new Lamborghini()
             {
+                Id = 3,
                 CarModel = "Lamborghini Huracan Performante Spyder",
                 YearModel = 2018,
                 DescriptionHtml = "<p>Leaner, tighter and more rewarding than any Huracan before it, the Performante was the V10 Lamborghini we had been waiting for. Now, the Spyder has received the very same treatment. The 5.2-litre V10 has been wrung out to 631bhp, while torque jumps up from 413 to 443lb ft. The weight saving is 35kg like for like, mostly down to the use of a lightweight exhaust system and what Lamborghini calls Forged Composites, which to you and I looks like chopped rather than woven carbon fibre. The chassis has been completely reworked for sharper, more focussed dynamics and the tyres are stupendously grippy Pirelli P Zero Corsas.<strong>AutoCar</strong>.</p>",
@@ -87,15 +90,17 @@ namespace LamborghiniGallery.Data
         {
             Lamborghini lamborghiniToReturn = null;
 
-            foreach (var lamborghini in _lamborghini)
-            {
-                if (lamborghini.Id == id)
+            foreach (var lamborghini in _lamborghini)//Foreach loop will go through all elements in array. The variable you define for the loop (lamborghini) will be set to one of the elements of the array. 
+            {                                       //So the first time the loop runs lamborghini, lamborghini will be set to the first item in the private array _lamborghini. Second time it will be set to the second item etc. 
+                if (lamborghini.Id == id) //If statement checking if lamborghini item id property equals the id passed into method. 
                 {
                     lamborghiniToReturn = lamborghini; //Matches id we're looking for to id inputted. So if 1 is performante and you type in 1 it will check it's matched. 
-                    break;
+                    break; //<-- breaks out of loop when matching id is found.
+                    //If id is matched lamborghiniToReturn wont be null it'll be a value matched in _lamborghini. 
+
                 }
             }
-            return lamborghiniToReturn;
+            return lamborghiniToReturn; //<- if loop finishes then means that none of the lamborghini in the items array had an id that matched what was passed into method, so will return null.
             
         }
 
